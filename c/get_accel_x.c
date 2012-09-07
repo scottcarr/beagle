b6a56662f1e2b4493ef38d84be056199a680e3d9
Scott Carr <carr27@purdue.edu>
2012-09-06 21:54:55 -0400

    added c files for reading i2c

---
+++ c/get_accel_x.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <linux/i2c-dev.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+
+int main() {
+    int file;
+    int adapter_nr = 3; /* probably dynamically determined */
+    char filename[20];
+
+    snprintf(filename, 19, "/dev/i2c-%d", adapter_nr);
+    file = open(filename, O_RDWR);
+    if (file < 0) {
+        printf("Opening i2c failed.\n");
+        exit(1);
+    }
+
+    int addr = 0x53 ; /* The I2C address */
+    if (ioctl(file, I2C_SLAVE, addr) < 0) {
+        /* ERROR HANDLING; you can check errno to see what went wrong */
+        exit(1);
+    }
+
+    char reg = 0x10;
+    int res;
+    char buf[2];
+    buf[0] = 0x32;
+
+    if (write(file,buf, 1) != 1) {
+        // error
+        printf("Reading i2c failed\n");
+    }
+
+    if (read(file,buf, 2) != 2) {
+        // error
+        printf("Reading i2c failed\n");
+    } else {
+        printf("Reading: %x %x\n", buf[1], buf[0]);
+    }
+
+    return 0;
+}
