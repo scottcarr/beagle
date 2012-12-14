b1c9be6ca7d53792fac97b9a34596ded30c546aa
Scott Carr <s.carr1024@gmail.com>
2012-12-14 12:26:45 -0500

    add year end report

--- fio/read_beagle.py
+++ fio/read_beagle.py
@@ -2,21 +2,34 @@ from pylab import *
 import numpy as np
 import scipy.fftpack as sf
 
-x = np.loadtxt("beagle_data2.csv", delimiter=',')
+def filter_same(data):
+    new = []
+    for i in range(len(data)-1):
+        if data[i] == data[i + 1]:
+            pass
+        else:
+            new.append(data[i])
+    return new
+
+#x = np.loadtxt("beagle_data3.csv", delimiter=',')
+x = np.loadtxt("sensor0.csv", delimiter=',')
 ion()
 figure(1)
 clf()
 t0 = x[0,0]
-plot(x[:,0] - t0, x[:,1], 'b-x')
-plot(x[:,0] - t0, x[:,2], 'g-x')
-plot(x[:,0] - t0, x[:,3], 'r-x')
+#plot(x[:,0] - t0, filter_same(x[:,1]), 'b-x')
+#plot(x[:,0] - t0, filter_same(x[:,2]), 'g-x')
+#plot(x[:,0] - t0, filter_same(x[:,3]), 'r-x')
+plot(filter_same(x[:,1]), 'b-x')
+plot(filter_same(x[:,2]), 'g-x')
+plot(filter_same(x[:,3]), 'r-x')
 legend(['x', 'y', 'z'])
 ylabel('accel (g)')
 xlabel('time(s)')
 
 figure(2)
 clf()
-s = sf.fft(x[:,3])
+s = sf.fft(x[:,1])
 freqs = sf.fftfreq(s.size, mean(np.diff(x[:,0])))
 zero = len(freqs)/2
 plot(freqs[:zero], abs(s[:zero]))
---
+++ year_end.md
@@ -0,0 +1,12 @@
+#Fall 2012 End of Semester
+
+Gustavo and Scott went to PCSI to do a data collection.
+
+We put the BeagleBone inside the central cap where the blades come together.
+
+[beaglebone mounting](beagle.jpg)
+
+We attached the sensors similarly to how the accelerameters were attached
+to the original turbine.
+
+[sensors on blade](blade.JPG)
